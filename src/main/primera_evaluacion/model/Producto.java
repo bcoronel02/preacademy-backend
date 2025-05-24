85516fa59e259c58b50ba2c5c0c74fef762ca188
Brenda Coronel <bcoronel4416@gmail.com>
2025-05-23 22:04:28 -0300

    se agrego la estructura base del proyecto, clase Carrito, Cliente, Cupon, ItemCarrito, Usuario, Producto y CarritoService

--- src/main/primera_evaluacion/model/Producto.java
+++ src/main/primera_evaluacion/model/Producto.java
@@ -1,4 +1,9 @@
 package main.primera_evaluacion.model;
 
 public class Producto {
+
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ src/main/primera_evaluacion/model/Usuario.java
@@ -0,0 +1,4 @@
+package main.primera_evaluacion.model;
+
+public class Usuario {
+}
