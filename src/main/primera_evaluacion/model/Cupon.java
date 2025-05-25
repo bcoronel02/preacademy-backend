baf187e52a1ea06b873b0993e92446d6a6c6ab2a
Brenda Coronel <bcoronel4416@gmail.com>
2025-05-25 20:59:01 -0300

    agrega a Cupon, getters y setters

--- src/main/primera_evaluacion/model/Cupon.java
+++ src/main/primera_evaluacion/model/Cupon.java
@@ -1,4 +1,30 @@
 package main.primera_evaluacion.model;
 
 public class Cupon {
+    private String codigo;
+    private double porcentajeDescuento;
+
+    // Constructor vacío
+    public Cupon() {
+
+    }
+
+    // Getters y setters
+    public String getCodigo() {
+        return codigo;
+
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+
+    }
+ 
+    public double getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    public void setPorcentajeDescuento(double porcentajeDescuento) {
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
 }
