91f5d1ed5a21b5a7438ab343ba53c04f499964bc
Brenda Coronel <bcoronel4416@gmail.com>
2025-05-26 15:00:20 -0300

    feat: agregar inicializacion de productos con el patron Builder, se agregaron tres productos y se utiliza builder para instanciar Producto.

--- src/main/primera_evaluacion/service/CarritoService.java
+++ src/main/primera_evaluacion/service/CarritoService.java
@@ -1,7 +1,31 @@
 package main.primera_evaluacion.service;
 
+import main.primera_evaluacion.model.*;
+
 public class CarritoService {
     public static void main(String[] args) {
+        // Crear productos con Builder
+        Producto producto1 = new Producto.Builder()
+                .id(1)
+                .nombre("Smartphone")
+                .precio(25000)
+                .categoria("Electrónicos")
+                .build();
+
+        Producto producto2 = new Producto.Builder()
+                .id(2)
+                .nombre("Notebook")
+                .precio(50000)
+                .categoria("Electrónicos")
+                .build();
+
+        Producto producto3 = new Producto.Builder()
+                .id(3)
+                .nombre("Auriculares")
+                .precio(5000)
+                .categoria("Accesorios")
+                .build();
+
 
     }
 }
