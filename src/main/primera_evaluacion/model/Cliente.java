cf2962a8ec6e9b2ddaa25a16ea4389df9161dc7e
Brenda Coronel <bcoronel4416@gmail.com>
2025-05-25 21:05:07 -0300

    agrega a Cliente, getters y setters

--- src/main/primera_evaluacion/model/Cliente.java
+++ src/main/primera_evaluacion/model/Cliente.java
@@ -1,4 +1,18 @@
 package main.primera_evaluacion.model;
 
-public class Cliente {
+public class Cliente extends Usuario {
+    private String nombreCompleto;
+
+    // Constructor vacío
+    public Cliente() {
+    }
+
+    // Getter y setter
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
 }
