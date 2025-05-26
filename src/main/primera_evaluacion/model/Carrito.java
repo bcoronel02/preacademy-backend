a512f0be815c8a75511cb92db8df95bb1bb3d79c
Brenda Coronel <bcoronel4416@gmail.com>
2025-05-26 14:00:14 -0300

    chore: agregar importación de Collectors para operaciones con streams, mejora funcionalidad de obtenerItemPorCategoria()

--- src/main/primera_evaluacion/model/Carrito.java
+++ src/main/primera_evaluacion/model/Carrito.java
@@ -2,7 +2,7 @@ package main.primera_evaluacion.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 public class Carrito {
     private List<ItemCarrito> items;
